e78af8be18a0391a97465e79c358e862f934e674
planeturban <ube@alienautopsy.net>
2014-11-13 21:51:31 +0100

    Added ini file. Added some sanity checks for inbound data.

--- marainer.py
+++ marainer.py
@@ -8,20 +8,30 @@ import serial
 from math import radians, cos, sin, asin, sqrt
 import sqlite3
 import time
-
+import configparser
 
 class Marainer():
-
     def __init__(self):
         self.stdin_path = '/dev/null'
         self.stdout_path = '/dev/tty'
         self.stderr_path = '/dev/tty'
         self.pidfile_path = '/tmp/marainer.pid'
         self.pidfile_timeout = 5
+        ini = configparser.ConfigParser()
+        home = os.path.expanduser("~")
+        inifiles = ["/etc/marainer/marainer.ini", home+"/.config/marainer.ini",
+                    home+"/.marainer/marainer.ini", "marainer.ini"]
+
+        for ifile in inifiles:
+            if os.path.isfile(ifile):
+                inifile = ifile
+        ini.read(inifile)
+        self.dbfile = ini['main']['db']
+        self.lockonstart = ini['main']['lockonstart']
+
 
     def run(self):
-        firstrun = True
-        db = sqlite3.connect("/home/pi/marainer.db")
+        db = sqlite3.connect(self.dbfile)
         c = db.cursor()
         config = {}
         updateTime = int(time.time())
@@ -45,6 +55,8 @@ class Marainer():
                 if data.startswith("$GPRMC"):
                     t = time.time()
                     a = data.strip().split(",")
+                    if len(a) != 13:
+                        continue
                     latPrefix = ""
                     lonPrefix = ""
                     valid = False
@@ -57,20 +69,21 @@ class Marainer():
                     if valid:
                         lat = round(float(a[3][:2]) + (float(a[3][2:]) * 60 / 3600), 6)
                         lon = round(float(a[5][:3]) + (float(a[5][3:]) * 60 / 3600), 6)
-                        if int(config['lock']) or firstrun:
+                        if int(config['lock']) or self.lockonstart:
                             c.execute("update config set value = '%f' where key = 'lockLat'" % lat)
                             c.execute("update config set value = '%f' where key = 'lockLon'" % lon)
                             c.execute("update config set value = '0' where key = 'lock'" )
                             config['lock'] = "0"
-                            firstrun = False
+                            self.lockonstart = False
                             db.commit()
                         c.execute("select * from location")
-                        posData = c.fetchone()
                         diff = self.haversine(lon, lat, float(config['lockLon']), float(config['lockLat']))
                         if diff > allowedDiff and int(config['armed']):
-                            url = "http://maps.google.com/maps?z=12&t=m&q=loc:%s%f+%s%f" % (latPrefix, lat, lonPrefix, lon)
-                            c.execute("insert into alarm (time, lon, lat, url, ack) values (%d, %f, %f, '%s', 0)" % (t, lon, lat, url));
-                        c.execute("update location set lon = %f, lat = %f" % (lon, lat))
+                            url = "http://maps.google.com/maps?z=12&t=m&q=loc:%s%f+%s%f" % (latPrefix, lat,
+                                                                                            lonPrefix, lon)
+                            c.execute("insert into alarm (time, lon, lat, url, ack) values (%d, %f, %f, '%s', 0)"§
+                                      % (t, lon, lat, url))
+                        c.execute("update location set lon = %f, lat = %f, time = %d" % (lon, lat, time.time()))
                         db.commit()
                     time.sleep(float(config['interval']))
 
